83905860aed975a14e2c5cc688bead22da77466b
Danielocho <danielnochess@gmail.com>
2022-04-13 11:34:41 -0500

    origin production

--- app.js
+++ app.js
@@ -16,11 +16,6 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(function(req, res, next) {
-    res.header('Access-Control-Allow-Origin', "*");
-    next();
-})
-
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
